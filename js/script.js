25475e50749f592449d0bb0d34ed372e1113038c
Kabahok <82036345+Kabahok@users.noreply.github.com>
2021-08-26 19:55:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let animItems = document.querySelectorAll(".anim");
+
+if(animItems.length > 0) {
+    window.addEventListener('scroll', animShow);
+    function animShow () {
+        for(let i = 0; i < animItems.length; i++) {
+            const animItem = animItems[i],
+                animItemHeight = animItem.offsetHeight,
+                animItemOffset = offset(animItem).top,
+                animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if ( window.innerHeight < animItemHeight) {
+                inimItemOffset = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if(pageYOffset > (animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add("active");
+            } else {
+                if(!animItem.classList.contains("no")) {
+                    animItem.classList.remove("active");
+                }
+                
+            }
+        }
+    }
+
+    animShow();
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXoffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {
+            top: rect.top + scrollTop, left: rect.left + scrollLeft
+        };
+    }
+}
+
+
+const siwper = new Swiper('.comment_container', {
+
+    slidesPerView: 1,
+    
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        576: {
+          slidesPerView: 1,  
+        },
+        // when window width is >= 640px
+        767: {
+          slidesPerView: 2
+        }
+      }
+});
