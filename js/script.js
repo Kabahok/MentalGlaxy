be928236d7b9e670ade5248f42ded944354a49e3
Kabahok <82036345+Kabahok@users.noreply.github.com>
2021-08-27 22:40:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -108,3 +108,62 @@ overlay.addEventListener('click', () => {
     document.body.style.overflow = '';
     document.body.style.marginRight = `0px`;
 });
+
+
+let modalBtn = document.querySelector('.modal_btn'),
+modalInput = document.querySelectorAll('.modal_input'),
+inputs = document.querySelectorAll('.input');
+
+modalBtn.addEventListener('click', () => {
+    inputs.forEach((item, i) => {
+        if(item.value == '') {
+            if(!item.classList.contains('req')) {
+                item.classList.add('req');
+            }
+        }
+    });
+});
+
+
+inputs.forEach((item, i) => {
+    item.addEventListener('input', () => {
+        if(!item.value == '') {
+            if(item.classList.contains('req')) {
+                item.classList.remove('req');
+            }
+        } else if (item.value == '') {
+            if(!item.classList.contains('req')) {
+                item.classList.add('req');
+            }
+        }
+    });
+});
+
+function sendForm() {
+    let modalForm = document.querySelector('.modal_form');
+
+    const postData = async (url, data) => {
+        const rec = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
+
+        return rec.text();
+    };
+
+    modalForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        
+        let formData = new FormData(modalForm);
+
+        postData('../server.json', formData).then((rec) => {
+            console.log(rec);
+            console.log('work');
+        }).catch( () => console.log('fail')).finally(() => {
+            console.log('Script is work');
+        });
+        
+    });
+}
+sendForm();
